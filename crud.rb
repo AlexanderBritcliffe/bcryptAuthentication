1e652eaca23c6d55f3d5b019d9524f4f1eef1b7c
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-08-03 00:33:22 -0400

    turned user passwords into hash

---
+++ crud.rb
@@ -0,0 +1,28 @@
+require 'bcrypt'
+
+users = [
+    { username: "mashur", password: "password1" },
+    { username: "jack", password: "password2" },
+    { username: "arya", password: "password3" },
+    { username: "jonshow", password: "password4" },
+    { username: "heisenburg", password: "password5" }
+]
+
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+puts create_secure_users(users)
+
+# 19 through 21 takes in each users password and turns it into hash
